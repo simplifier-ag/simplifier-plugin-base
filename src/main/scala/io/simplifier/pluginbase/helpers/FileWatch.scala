05d381db5e36ba812074c242398f9a683602f948
Volker <volker.vaeth@simplifier.io>
2026-02-09 16:58:53 +0100

    35302 - applying filewatch filter before watching

--- src/main/scala/io/simplifier/pluginbase/helpers/FileWatch.scala
+++ src/main/scala/io/simplifier/pluginbase/helpers/FileWatch.scala
@@ -1,6 +1,7 @@
 package io.simplifier.pluginbase.helpers
 
 import io.simplifier.pluginbase.util.logging.Logging
+import org.apache.commons.io.filefilter.{FileFilterUtils, IOFileFilter}
 import org.apache.commons.io.monitor.{FileAlterationListener, FileAlterationListenerAdaptor, FileAlterationMonitor, FileAlterationObserver}
 
 import java.io.File
@@ -9,24 +10,27 @@ trait FileWatch extends Logging {
 
   def onChange(file: File): Unit
 
-  def watchFile(fileToWatch: File) {
-    val parentDirectory: File = fileToWatch match {
-      case file if file.isFile => file.getParentFile
-      case directory if directory.isDirectory => directory
-    }
-    val observer: FileAlterationObserver = new FileAlterationObserver(parentDirectory)
-    val monitor: FileAlterationMonitor = new FileAlterationMonitor()
-    val listener: FileAlterationListener = new FileAlterationListenerAdaptor {
-      override def onStart(observer: FileAlterationObserver): Unit = {
-        super.onStart(observer)
-      }
+  def watchFile(fileToWatch: File): Unit = {
+    val (directory, fileFilter): (File, IOFileFilter) = fileToWatch match {
+      case file if file.isFile =>
+        // For a specific file: watch parent directory, but filter for only this file
+        val filter = FileFilterUtils.nameFileFilter(file.getName)
+        (file.getParentFile, filter)
 
+      case directory if directory.isDirectory =>
+        // For a directory: watch it and accept all files in it
+        (directory, FileFilterUtils.trueFileFilter())
+    }
+
+    // Create observer with file filter - only monitors files matching the filter
+    val observer: FileAlterationObserver = new FileAlterationObserver(directory, fileFilter)
+    val monitor: FileAlterationMonitor = new FileAlterationMonitor()
+
+    val listener: FileAlterationListener = new FileAlterationListenerAdaptor {
       override def onFileChange(file: File): Unit = {
         super.onFileChange(file)
-        if (fileToWatch.isFile && file.getPath.equals(fileToWatch.getPath))
-          onChange(file)
-        else if (fileToWatch.isDirectory)
-          onChange(file)
+        // No filtering needed here anymore - the observer already filtered for us
+        onChange(file)
       }
     }
 
